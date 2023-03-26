098099800fb6b47fc5079307bbd73c3c5829debf
aidensane <98586059+aidensane@users.noreply.github.com>
2023-03-26 19:48:10 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,12 +2,47 @@ import curses
 from curses import wrapper
 
 
-def main(stdscr):
-    curses.init_pair(1, curses.COLOR_GREEN, curses.COLOR_WHITE)
+def start_screen(stdscr):
     stdscr.clear()
-    stdscr.addstr("Hello world")
+    stdscr.addstr("Welcome to the speed typing test!", curses.color_pair(1))
+    stdscr.addstr("\nPress any key to begin ", curses.color_pair(1))
     stdscr.refresh()
     stdscr.getkey()
+
+
+def wpm_test(stdscr):
+    target_text = "Hello world this is some test text for this app!"
+    current_text = []
+
+    stdscr.clear()
+    stdscr.addstr(target_text)
+    stdscr.refresh()
+
+    while True:
+        stdscr.clear()
+        stdscr.addstr(target_text)
+
+        for char in current_text:
+            stdscr.addstr(char, curses.color_pair(1))
+
+        stdscr.refresh()
+
+        key = stdscr.getkey()
+
+        if ord(key) == 27:
+            break
+
+        current_text.append(key)
+
+
+
+def main(stdscr):
+    curses.init_pair(1, curses.COLOR_GREEN, curses.COLOR_BLACK)
+    curses.init_pair(2, curses.COLOR_RED, curses.COLOR_BLACK)
+    curses.init_pair(2, curses.COLOR_WHITE, curses.COLOR_BLACK)
+
+    start_screen(stdscr)
+    wpm_test(stdscr)
 
 
 wrapper(main)
